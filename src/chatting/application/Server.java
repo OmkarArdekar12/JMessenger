d37706195de29b399e81a67c454caa2ec386c440
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-12-05 17:31:24 +0530

    Messenger App Header Completed

--- src/chatting/application/Server.java
+++ src/chatting/application/Server.java
@@ -27,11 +27,32 @@ public class Server extends JFrame implements ActionListener {
         p1.add(back);
 
         ImageIcon u1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/user1.png"));
-        Image u2 = u1.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        Image u2 = u1.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
         ImageIcon u3 = new ImageIcon(u2);
         JLabel userProfile = new JLabel(u3);
         userProfile.setBounds(45, 10, 50, 50);
         p1.add(userProfile);
+
+        ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/call.png"));
+        Image c2 = c1.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon c3 = new ImageIcon(c2);
+        JLabel phoneCall = new JLabel(c3);
+        phoneCall.setBounds(315, 20, 30, 30);
+        p1.add(phoneCall);
+
+        ImageIcon v1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
+        Image v2 = v1.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon v3 = new ImageIcon(v2);
+        JLabel videoCall = new JLabel(v3);
+        videoCall.setBounds(355, 20, 30, 30);
+        p1.add(videoCall);
+
+        ImageIcon m1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/more.png"));
+        Image m2 = m1.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon m3 = new ImageIcon(m2);
+        JLabel moreOptions = new JLabel(m3);
+        moreOptions.setBounds(400, 20, 30, 30);
+        p1.add(moreOptions);
 
         setSize(450, 700);
         setLocation(200, 50);
@@ -41,6 +62,7 @@ public class Server extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae) {
+
     }
 
     public static void main(String args[]) {
